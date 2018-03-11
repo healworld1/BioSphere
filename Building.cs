afafedef7fdb607498f81e0851a1765fd3677faf
cangular <32854217+cangular@users.noreply.github.com>
2018-03-11 00:25:04 -0500

    Add files via upload

---
+++ Building.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface Building : MonoBehaviour {
+	int sizeOccupied {
+		get;
+		set;
+	}   // return the size a building will occupied
+	int cost {
+		get;
+		set;
+	}   // return the cost of the buildling;
+	string showProperty ()  // show the property a building will have in the text
+	bool availblility()// return the availbility of the building
+}

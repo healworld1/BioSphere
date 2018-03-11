5ec35acef4aa7dc10f32a7a91a2dc611c38dad0e
cangular <32854217+cangular@users.noreply.github.com>
2018-03-11 01:13:59 -0500

    abstract method for building
    abstract method for building

--- Assets/Scripts/Main/Building.cs
+++ Assets/Scripts/Main/Building.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 abstract class Building : MonoBehaviour {
-	int sizeOccupied {
-		get;
-		set;
-	}   // return the size a building will occupied
-	int cost {
-		get;
-		set;
-	}   // return the cost of the buildling;
-    abstract public string ShowProperty(); // show the property a building will have in the text
-
-    abstract public bool Availblility();// return the availbility of the building
+    abstract class Building : MonoBehaviour
+    {
+        public bool Availblility = false;// return the availbility of the building
+        public int sizeOccupied = 0;   // return the size a building will occupied
+        public int cost = 0;   // return the cost of the buildling;
+        abstract public string ShowProperty(); // show the property a building will have in the text
+    }
 }
